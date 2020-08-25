ccd9f2cf62c6fa7ce03a97169306327b7f1e23e9
Yerson Reyes <yerson.programador@gmail.com>
2020-08-25 18:41:59 -0400

    Arreglos en el index y Home y agregado archivo redirect

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -6,17 +6,20 @@ export default function Footer() {
     <div className="footer">
       <ul className="footer-list">
         <li>
-          <a href="">
+          <a href="https://twitter.com/YersonReyesDev" target="_blank">
             <i className="fab fa-twitter"></i>
           </a>
         </li>
         <li>
-          <a href="">
+          <a href="https://github.com/yersonreyes" target="_blank">
             <i className="fab fa-github"></i>
           </a>
         </li>
         <li>
-          <a href="">
+          <a
+            href="https://www.linkedin.com/in/yerson-reyes-rosales/"
+            target="_blank"
+          >
             <i className="fab fa-linkedin-in"></i>
           </a>
         </li>
